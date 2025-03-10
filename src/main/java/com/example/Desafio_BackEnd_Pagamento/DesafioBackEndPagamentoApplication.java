ad5c9197d2361e57e4ee1f4cf7c84dc0874d8e81
JulianaNishimura <juliananishimura.tds24@gmail.com>
2025-03-10 16:51:21 -0300

    pastas mvc

---
+++ src/main/java/com/example/Desafio_BackEnd_Pagamento/DesafioBackEndPagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Desafio_BackEnd_Pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackEndPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackEndPagamentoApplication.class, args);
+	}
+
+}
